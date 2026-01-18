2c58144c2307a26ac6156d9f8bbd2b672cc0ca2e
dcx2024 <166218735+dcx2024@users.noreply.github.com>
2026-01-18 17:53:29 +0100

    Switch from createBrowserRouter to createHashRouter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+// 1. Change createBrowserRouter to createHashRouter
+import { Route, createHashRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import Explore from './pages/ExplorePage';
 
 const App = () => {
-  const router = createBrowserRouter(
+  // 2. Update the function call here
+  const router = createHashRouter(
     createRoutesFromElements(
       <>
         {/* Main Landing Page: Displays Recent 20 */}
