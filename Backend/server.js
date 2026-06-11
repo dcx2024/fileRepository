37f0f65e208e8f1a5983e369c0bc4045fc6bca6c
dcx2024 <166218735+dcx2024@users.noreply.github.com>
2026-06-11 16:46:48 +0100

    Add cookie parser middleware to server

--- Backend/server.js
+++ Backend/server.js
@@ -6,13 +6,14 @@ const admin = require('./Routes/uploadRoute')
 const exam= require('./Routes/examRoute')
 const auth = require('./Routes/authRoute')
 app.use(express.json())
+app.use(cookieParser())
 app.use(cors({
     origin:'https://filerepository-frontend.onrender.com',
     credentials:true
 }))
 
 app.use('/uploads', express.static('public/uploads'));
-app.use(cookieParser())
+
 app.use('/api/admin',admin)
 app.use('/api/exam',exam)
 app.use('/api/auth',auth)
